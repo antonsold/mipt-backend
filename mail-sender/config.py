4a26b6de427fa0fb9827243764dad5fde7aa68de
anton <soldatenkovanton@mail.ru>
2019-05-19 16:51:35 +0300

    done

---
+++ mail-sender/config.py
@@ -0,0 +1,24 @@
+import os
+
+class Config(object):
+    # main config
+    SECRET_KEY = 'my_precious'
+    SECURITY_PASSWORD_SALT = 'my_precious_two'
+    DEBUG = False
+    BCRYPT_LOG_ROUNDS = 13
+    WTF_CSRF_ENABLED = True
+    DEBUG_TB_ENABLED = False
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+    # mail settings
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+
+    # gmail authentication
+    MAIL_USERNAME = 'kek'
+    MAIL_PASSWORD = 'abacaba2'
+
+    # mail accounts
+    MAIL_DEFAULT_SENDER = 'buchnev.vs@phystech.edu'
